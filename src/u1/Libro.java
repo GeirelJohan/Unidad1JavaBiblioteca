01c08f44b7fe5d5144917ee016e003ad1495eb64
Geirel <Geirel@DESKTOP-5H54R8H>
2026-05-12 19:58:29 -0600

    Enumeraciones (enum)

--- src/u1/Libro.java
+++ src/u1/Libro.java
@@ -12,30 +12,42 @@ public class Libro {
     //Atributos privados (encapsulación)
     private String titulo;
     private String autor;
+    private Genero genero;
 
     //Constructor
-    public Libro(String titulo, String autor) {
+    public Libro(String titulo, String autor, Genero genero) {
         this.titulo = titulo;
         this.autor = autor;
+        this.genero = genero;
     }
 
     //Metodos de acceso (getters y setters)
+    //Titulo
     public String getTitulo() {
         return titulo;
     }
     public void setTitulo(String titulo) {
         this.titulo = titulo;
     }
-
+    //Autor
     public String getAutor() {
         return autor;
     }
     public void setAutor(String autor) {
         this.autor = autor;
     }
+    //Genero
+    public Genero getGenero() {
+        return genero;
+    }
+    public void setGenero (Genero genero) {
+        this.genero = genero;
+    }
     
+    //Mostrar el titulo y autor
     public void mostrarInfo(){
         System.out.println("Título:" + this.titulo);
         System.out.println("Autor:" + this.autor);
+        System.out.println("Genero:" + this.genero);
     }
 }
